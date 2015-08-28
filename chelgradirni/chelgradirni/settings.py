735c0c2a2b0c1750f1c6b9b4e78ad1c4b409fc40
Viktor Ikonnikov <viktor.ikonnikov@gmail.com>
2015-08-29 00:01:06 +0500

    STATIC_ROOT defined

--- chelgradirni/chelgradirni/settings.py
+++ chelgradirni/chelgradirni/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Quick-start development settings - unsuitable for production
