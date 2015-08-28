0e57215b7bf612b19ce455e9fa55385ecf8e27ca
Viktor Ikonnikov <viktor.ikonnikov@gmail.com>
2015-08-28 23:10:53 +0500

    App core initialized

--- chelgradirni/core/urls.py
+++ chelgradirni/core/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+from django.views.generic.base import TemplateView
+
+import core
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^/', include('core.urls'))
+    url(r'^$', TemplateView.as_view(template_name='core/index.html')),
 ]
