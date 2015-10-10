f54ce244324d5744a2d9ff19b0c1301c29fde221
viktor <viktor.ikonnikov@gmail.com>
2015-10-10 23:03:32 +0500

    Model Cooler added.

--- chelgradirni/core/models.py
+++ chelgradirni/core/models.py
@@ -1,3 +1,14 @@
+# -*- coding: utf8 -*-
+
 from django.db import models
 
 # Create your models here.
+
+class Cooler(models.Model):
+    name = models.CharField(u'Название', blank=True, max_length=10)
+    performance = models.CharField(u'Производительность', blank=True, max_length=5)
+    load = models.CharField(u'Тепловая нагрузка', blank=True, max_length=5)
+    drop = models.CharField(u'Перепад температур', blank=True, max_length=5)
+    power = models.CharField(u'Мощность вентелятора', blank=True, max_length=5)
+    weight = models.CharField(u'Масса', blank=True, max_length=5)
+    dimension = models.CharField(u'Габаритные размеры', blank=True, max_length=15)
